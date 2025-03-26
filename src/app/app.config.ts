d5c2923da951b932c8b3aa6a36dfdf7dece60505
Rajan Singh <rajansingh21@gmail.com>
2025-03-26 19:27:45 +0000

    app config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,26 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter, withViewTransitions } from '@angular/router';
+import {
+  provideRouter,
+  withPreloading,
+  withViewTransitions,
+} from '@angular/router';
 
 import { routes } from './app.routes';
 import { NameService } from './features/demos/services/name';
 import { PrefsStore } from './services/prefs.store';
+import { CustomPreloadingStrategy } from '@app-shared/router';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    CustomPreloadingStrategy,
     provideZoneChangeDetection({ eventCoalescing: true }),
-
-    provideRouter(routes, withViewTransitions()),
+    provideHttpClient(),
+    provideRouter(
+      routes,
+      withViewTransitions(),
+      withPreloading(CustomPreloadingStrategy),
+    ),
     NameService, // this is the "global instance" and the only one, unless anyone else provides it.
     PrefsStore, // This does NOT create this, you can use a factory, but that's hard.
   ],
